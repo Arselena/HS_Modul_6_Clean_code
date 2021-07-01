607a3df2e600aad375336b82ec2392263dff115b
Arselena <efedorovyh@yandex.ru>
2021-07-01 21:54:25 +0500

    6.7. ООП и интерфейсы

---
+++ Level_6_7.py
@@ -0,0 +1,10 @@
+# 6.7. ООП и интерфейсы   # У меня нет подходящего кода, но думаю это могло выглядеть так:
+# 1. Сделайте в своём коде три примера наглядных методов-фабрик
+1. Color tomato = Color.makeFromPalette(255, 99, 71)
+2. Caffe late = Caffe.makeFromType("Latte")
+3. Data day_of_the_week = Date.makeFromCalender(data)
+
+# 2. Если вы когда-нибудь использовали интерфейсы или абстрактные классы, напишите несколько примеров их правильного именования.
+1. interface Color 
+2. interface Coffe
+3. interface Data
